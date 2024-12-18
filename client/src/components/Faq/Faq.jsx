d2085803ef7049768ba263b5a2f2db8205a564ef
Subhrajit Talukdar <117500946+subhrajit77@users.noreply.github.com>
2024-12-18 21:48:17 +0530

    update: faq

--- client/src/components/Faq/Faq.jsx
+++ client/src/components/Faq/Faq.jsx
@@ -2,67 +2,70 @@ import { useState } from "react";
 import classes from "./Faq.module.css";
 
 const questions = [
-  {
-    question: "What is SAMBHAV 2K23?",
-    answer:
-      "SAMBHAV 2k23 is the one of biggest Technical event in the history of DYPCOE.",
-  },
-  {
-    question: "What are the Dates and Timings of event?",
-    answer:
-      "The event will start at 9AM on 20th Feb and will be till 21st of Feb 10 PM",
-  },
-  {
-    question: "How can I participate in the event?",
-    answer:
-      "Explore various events on the SAMBHAV's website and register for the same",
-  },
-  {
-    question: "Is there any entry fee for SAMBHAV 2k23?",
-    answer:
-      "No, there is not any entry fee to explore SAMBHAV 2k23 but to participate in some events you have pay registration fee.",
-  },
+    {
+        question: "How can I join Astrophysics club?",
+        answer: "You can join the Astrophysics Club by signing up during membership drives or attending our orientation sessions. Stay updated through our social media!",
+    },
+    {
+        question: "Do I need prior knowledge to join?",
+        answer: "No, the club is open to all levels of interest, from beginners to space enthusiasts.",
+    },
+    {
+        question: "What does the club organize? ",
+        answer: "The club hosts stargazing sessions, workshops, guest lectures, and fun discussions on celestial phenomena.",
+    },
+    {
+        question: "How can I participate in the event?",
+        answer: "Explore various events on the club's website or social media pages and register for the same",
+    },
 ];
 
 const Faq = () => {
-  const [clicked, setClicked] = useState(null);
+    const [clicked, setClicked] = useState(null);
 
-  const toggle = (i) => {
-    if (clicked === i) {
-      return setClicked(null);
-    }
+    const toggle = (i) => {
+        if (clicked === i) {
+            return setClicked(null);
+        }
 
-    setClicked(i);
-  };
+        setClicked(i);
+    };
 
-  return (
-    <section className={classes.faqSection}>
-      <div className={classes.heading}>FAQ</div>
-      <div className={classes.faq}>
-        {questions.map((ques, i) => {
-          return (
-            <div className={classes.single} onClick={() => toggle(i)}>
-              <div className={classes.question}>{ques.question}</div>
-              <div
-                className={`${
-                  clicked === i ? classes.answer : classes.noAnswer
-                }`}
-              >
-                {ques.answer}
-              </div>
-              <span className={classes.btn}>+</span>
-            </div>
-          );
-        })}
+    return (
+        <section className={classes.faqSection}>
+            <div className={classes.heading}>FAQ</div>
+            <div className={classes.faq}>
+                {questions.map((ques, i) => {
+                    return (
+                        <div
+                            className={classes.single}
+                            onClick={() => toggle(i)}
+                        >
+                            <div className={classes.question}>
+                                {ques.question}
+                            </div>
+                            <div
+                                className={`${
+                                    clicked === i
+                                        ? classes.answer
+                                        : classes.noAnswer
+                                }`}
+                            >
+                                {ques.answer}
+                            </div>
+                            <span className={classes.btn}>+</span>
+                        </div>
+                    );
+                })}
 
-        {/* <div className={classes.single}>
+                {/* <div className={classes.single}>
                 <div className={classes.question}>How are you?</div>
                 <div className={classes.answer}>I am fine</div>
                 <span className={classes.btn}>+</span>
             </div> */}
-      </div>
-    </section>
-  );
+            </div>
+        </section>
+    );
 };
 
 export default Faq;
