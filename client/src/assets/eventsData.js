f4249d0bd19f309f32636a423367f1474886cf2b
Subhrajit Talukdar <117500946+subhrajit77@users.noreply.github.com>
2025-02-15 12:02:19 +0530

    update: event page

--- client/src/assets/eventsData.js
+++ client/src/assets/eventsData.js
@@ -1,6 +1,7 @@
 import DebateCompetition from "./Events/Debate-Competition.jpg";
 import Chandrayaan3 from "./Events/Chandrayaan3-Landing.png";
 import EssayCompetition from "./Events/Essay-Competition-2023.png";
+import AstrophysicsTalk from "./Events/Astrophysics-talk.png";
 
 export const eventsData = [
     {
@@ -28,7 +29,7 @@ export const eventsData = [
     {
         id: 3,
         image: EssayCompetition,
-        name: "Essay Competition: Chandrayaan's Impact",
+        name: "ESSAY COMPETITION",
         description:
             "Express your thoughts and insights on the profound impact of the Chandrayaan mission! Participate in our essay competition and explore the scientific, technological, and societal significance of India's lunar achievement. Share your unique perspective and contribute to the celebration of this historic event.",
         rules: [
@@ -49,32 +50,22 @@ export const eventsData = [
         ],
         location: "BKB Seminar Hall",
         date: "20th FEB",
-        note: [""]
+        note: [""],
     },
+
     {
         id: 4,
-        image: EssayCompetition,
-        name: "Essay Competition: Chandrayaan's Impact",
+        image: AstrophysicsTalk,
+        name: "ASTROPHYSICS TALK",
         description:
-            "Express your thoughts and insights on the profound impact of the Chandrayaan mission! Participate in our essay competition and explore the scientific, technological, and societal significance of India's lunar achievement. Share your unique perspective and contribute to the celebration of this historic event.",
-        rules: [
-            "Essays must be submitted by [Date/Time].",
-            "Essays should focus on the impact and significance of the Chandrayaan mission.",
-            "Word limit: [Specify word limit].",
-            "Essays will be judged on content, clarity, originality, and adherence to the theme.",
-            "Submission format: [Specify format, e.g., PDF, DOC].",
-            "Plagiarism will result in disqualification.",
-        ],
+        "Join us for an enlightening Astrophysics Talk as part of our Chandrayaan Utsav! Explore the fascinating interplay between the Sun and Earth through the lens of solar wind plasma. Delve into the science behind coronal mass ejections, geomagnetic storms, and their impact on our planet. Gain insights from expert speakers and unravel the mysteries of our Sun-Earth connection.",
 
-        prizes: ["EXICITING PRIZES AND REWARDS"],
-        team: "Individual",
-        fees: "Free",
         contactInfo: [
-            "Vedika Sangle : 9578907935",
-            "Utkarsha Kesarkar : 7887440601",
+            "Vedika Sangle: 9578907935",
+            "Utkarsha Kesarkar: 7887440601",
         ],
         location: "BKB Seminar Hall",
-        date: "20th FEB",
-        note: [""]
+        date: "11th Nov 2022",
+        note: ["Seating is limited. Please arrive early."],
     },
 ];
